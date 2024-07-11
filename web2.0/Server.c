aaef8ed991857fb3f1fc6eb6fb299e0df1e072b1
cw <1206072169@qq.com>
2024-07-11 21:06:35 +0800

    web2.0

--- web2.0/Server.c
+++ web2.0/Server.c
@@ -4,6 +4,14 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <strings.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <assert.h>
+#include <sys/sendfile.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 int initListenFd(unsigned short port)
 {
@@ -119,7 +127,7 @@ int recvHttpRequest(int cfd, int epfd)
 {
     int len = 0, totle = 0;;
     char tmp[1024] = { 0 };
-    char buf[4096] = {0};
+    char buf[4096] = { 0 };
     while(len = recv(cfd, tmp, sizeof(tmp), 0) > 0)
     {
         if(totle + len < sizeof buf)
@@ -132,6 +140,10 @@ int recvHttpRequest(int cfd, int epfd)
     if(len == -1 && errno == EAGAIN)
     {
         //解析请求行
+        char* pt = strstr(buf, "\r\n");
+        int reqLen = pt - buf;
+        buf[reqLen] = '\0';
+        parseRequestLine(buf, cfd);
 
     }
     else if (len == 0)
@@ -145,3 +157,192 @@ int recvHttpRequest(int cfd, int epfd)
         perror("recv");
     }
 }
+
+
+int parseRequestLine(const char *line, int cfd)
+{
+    //解析请求行    get /xxx/1.jpg http/1.1
+    char method[12];
+    char path[1024];
+    sscanf(line, "%[^ ] %[^ ]", method, path);
+    if(strcasecmp(method, "get") != 0)  //不区分大小写
+    {
+        return -1;
+    }
+    //处理客户端请求的静态资源（目录或者文件）
+    char* file = NULL;
+    if(strcmp(path, "/") == 0)
+    {
+        file = "./";
+    }
+    else
+    {
+        file = path + 1;
+    }
+    //获取文件属性
+    struct stat st;
+    int ret = stat(file, &st);
+    if(ret == -1)
+    {
+        //文件不存在--回复404
+        sendHeadMsg(cfd, 404, "Not Found", getFileType(".html"), -1);
+        sendFile("404.html", cfd);
+        return 0;
+    }
+    //判断文件类型
+    if(S_ISDIR(st.st_mode))     //如果是目录返回1
+    {
+        //把这个目录中的内容发送给客户端
+        sendHeadMsg(cfd, 200, "OK", getFileType("html"), -1);
+        sendDir(file,cfd);
+    }
+    else
+    {
+        //把文件的内容发送给客户端
+        sendHeadMsg(cfd, 200, "OK", getFileType(file), st.st_size);
+        sendFile(file, cfd);
+    }
+    return 0;
+}
+
+
+
+const char* getFileType(const char *name)
+{
+    //a.jpg  a.mp4  a.html
+    //自右向左查找'.'字符，如果不存在返回NULL
+    const char* dot = strrchr(name, '.');
+    if(dot == NULL)
+        return "text/plain; charset=utf-8";//纯文本
+    if (strcmp(dot, ".html") == 0 || strcmp(dot, ".htm") == 0)
+        return "text/html; charset=utf-8";
+    if (strcmp(dot, ".jpg") == 0 || strcmp(dot, ".jpeg") == 0)
+        return "image/jpeg";
+    if (strcmp(dot, ".gif") == 0)
+        return "image/gif";
+    if (strcmp(dot, ".png") == 0)
+        return "image/png";
+    if (strcmp(dot, ".css") == 0)
+        return "text/css";
+    if (strcmp(dot, ".au") == 0)
+        return "audio/basic";
+    if (strcmp(dot, ".wav") == 0)
+        return "audio/wav";
+    if (strcmp(dot, ".avi") == 0)
+        return "video/x-msvideo";
+    if (strcmp(dot, ".mov") == 0 || strcmp(dot, ".qt") == 0)
+        return "video/quicktime";
+    if (strcmp(dot, ".mpeg") == 0 || strcmp(dot, ".mpe") == 0)
+        return "video/mpeg";
+    if (strcmp(dot, ".vrml") == 0 || strcmp(dot, ".wrl") == 0)
+        return "model/vrml";
+    if (strcmp(dot, ".midi") == 0 || strcmp(dot, ".mid") == 0)
+        return "audio/midi";
+    if (strcmp(dot, ".mp3") == 0)
+        return "audio/mpeg";
+    if (strcmp(dot, ".ogg") == 0)
+        return "application/ogg";
+    if (strcmp(dot, ".pac") == 0)
+        return "application/x-ns-proxy-autoconfig";
+
+    return "text/plain; charset=utf-8";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int sendDir(const char *dirName, int cfd)
+{
+    char buf[4096] = {0};
+    sprintf(buf, "<html><head><title>%s</title></head><body><table>",dirName);
+    struct dirent** namelist;
+    int num = scandir(dirName, &namelist, NULL, alphasort);
+    for(int i = 0; i < num; ++i)
+    {
+        //取出文件名 namelist指向的是一个指针数组，struct dirent* tmp[]
+        char* name = namelist[i]->d_name;
+        struct stat st;
+        char subPath[1024] = {0};
+        sprintf(subPath, "%s/%s", dirName, name);
+        stat(subPath, &st);
+        if(S_ISDIR(st.st_mode))
+        {
+            //a标签<a href="">name</a>      实现跳转
+            sprintf(buf + strlen(buf), "<tr><td><a href=\"%s/\">%s</a></td><td>%ld</td></tr>",
+             name, name, st.st_size);
+        }
+        else
+        {
+            sprintf(buf + strlen(buf), "<tr><td><a href=\"%s\">%s</a></td><td>%ld</td></tr>",
+             name, name, st.st_size);
+        }
+        send(cfd,buf,strlen(buf), 0);
+        memset(buf,0, sizeof(buf));
+        free(namelist[i]);
+    }
+    sprintf(buf,"</table></body></html>");
+    send(cfd,buf,strlen(buf),0);
+    free(namelist);
+    return 0;
+}
+
+int sendFile(const char* fileName, int cfd)
+{
+    //1.打开文件
+    int fd = open(fileName, O_RDONLY);
+    assert(fd > 0); //如果小于0，则终止程序
+#if 0
+    while (1)
+    {
+        char buf[1024];
+        int len = read(fd, buf, sizeof buf);
+        if(len >0)
+        {
+            send(cfd,buf,len,0);
+            usleep(10);  //这非常重要
+        }
+        else if(len == 0)
+        {
+            break;
+        }
+        else
+        {
+            perror("read");
+        }
+    }
+#else
+    int size = lseek(fd, 0, SEEK_END);
+    sendfile(cfd, fd, NULL, size);
+#endif
+    return 0;
+}
+
+int sendHeadMsg(int cfd, int status, const char *descr, const char *type, int length)
+{
+    //状态行
+    char buf[4096] = { 0 };
+    sprintf(buf, "http/1.1 %d %s\r\n", status, descr);
+    //响应头
+    sprintf(buf + strlen(buf), "content-type: %s\r\n", type);
+    sprintf(buf + strlen(buf), "content-length: %d\r\n\r\n", length);
+
+    send(cfd,buf, strlen(buf), 0);
+    return 0;
+}
+
+
